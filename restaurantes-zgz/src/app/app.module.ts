af106a342f5fb9e1126c053551ac503da1f44a9a
Guillermo Sesé Santos <guillermosesesantos@hotmail.com>
2018-12-18 13:51:53 +0100

    añadida app

---
+++ restaurantes-zgz/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { HomePage } from '../pages/home/home';
+import { MapPage } from '../pages/map/map';
+import { SettingsPage } from '../pages/settings/settings';
+import { MytabsPage } from '../pages/mytabs/mytabs';
+
+import { MyApp } from './app.component';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    MapPage,
+    SettingsPage,
+    MytabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    MapPage,
+    SettingsPage,
+    MytabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
